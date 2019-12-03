85512a42bd4898dcc40a7fd428adb5f297392043
osmunaj <56142640+osmunaj@users.noreply.github.com>
2019-12-03 09:33:19 -0700

    Add files via upload

---
+++ apcsp_final.js
@@ -0,0 +1,12 @@
+function ready(){
+    console.log("Page Ready");
+
+    let se = document.forms.scale_evaluation;
+
+    let tonic = se.element.tonic;
+
+    //console.log(tonic.value)
+}
+
+document.addEventListener("DOMContentLoaded", ready);
+
